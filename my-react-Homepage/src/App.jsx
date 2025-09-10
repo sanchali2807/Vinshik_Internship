2644474bccfbb948f7f6ba4de84489b9e0309356
Sanchali <sanchali280705@gmail.com>
2025-09-10 16:50:53 +0530

    initial setup

---
+++ my-react-Homepage/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Card from "./Card";
+
+
+function App() {
+  return(
+    <>
+    <Header/>
+    <Card/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App
